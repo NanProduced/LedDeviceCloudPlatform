7e833a1350309a26931401a4f703b92beaba86ea
Nan <nanproduced@gmail.com>
2025-06-20 21:14:05 +0800

    pom

---
+++ auth-server/auth-common/src/main/java/org/nan/auth/common/Organization.java
@@ -0,0 +1,14 @@
+package org.nan.auth.common;
+
+import lombok.Data;
+
+@Data
+public class Organization {
+
+    private Long oid;
+
+    private String name;
+
+    private String description;
+    
+}
