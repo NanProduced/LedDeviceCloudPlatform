40041cbd50d354056e09bfb5180175a18a4e0cb1
Nan <nanproduced@gmail.com>
2025-07-08 10:45:28 +0800

    fix: temp

--- auth-server/auth-boot/src/main/java/org/nan/cloud/auth/boot/oidc/BackChannelLogoutHandler.java
+++ auth-server/auth-boot/src/main/java/org/nan/cloud/auth/boot/oidc/BackChannelLogoutHandler.java
@@ -13,10 +13,8 @@ import com.nimbusds.openid.connect.sdk.BackChannelLogoutRequest;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import lombok.val;
 import org.nan.cloud.auth.boot.config.OAuth2Constants;
 import org.nan.cloud.auth.boot.config.OAuth2ServerProps;
 import org.nan.cloud.auth.boot.oauth.OidcAuthorizationService;
@@ -39,7 +37,6 @@ import org.springframework.util.StringUtils;
 import java.io.IOException;
 import java.net.URI;
 import java.util.*;
-import java.util.stream.Collectors;
 
 @Slf4j
 public class BackChannelLogoutHandler implements AuthenticationSuccessHandler {
@@ -69,64 +66,74 @@ public class BackChannelLogoutHandler implements AuthenticationSuccessHandler {
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
 
-        // 是否开启SLO
-        if (!oAuth2ServerProps.getEnableOidcSLO()) {
-            log.debug("Custom-Debug-log===>user: {} log out, OIDC SLO not enable", authentication.getName());
-            return;
-        }
-
-        // OP 本地 Session 立即失效
-        new SecurityContextLogoutHandler().logout(request, response, authentication);
-
         // 已经验证通过的注销AuthorizationToken
         OidcLogoutAuthenticationToken logoutAuthenticationToken = (OidcLogoutAuthenticationToken) authentication;
         String sessionId = logoutAuthenticationToken.getSessionId();
-        OAuth2Authorization curOAuth2Authorization = oidcAuthorizationService.findByIdToken(logoutAuthenticationToken.getIdTokenHint());
-        if (null == curOAuth2Authorization) {
-            log.error("Can not find OAuth2Authentication for idToken!");
-            response.setStatus(HttpStatus.BAD_REQUEST.value());
-            response.getWriter().flush();
+        String idTokenHint = logoutAuthenticationToken.getIdTokenHint();
+        // 查询当前认证信息
+        OAuth2Authorization curOAuth2Authorization = oidcAuthorizationService.findByIdToken(idTokenHint);
+        // 查询调用SLO的客户端
+        RegisteredClient callingClient = registeredClientRepository.findById(curOAuth2Authorization.getRegisteredClientId());
+        if (null == callingClient) {
+            log.warn("Can't find current Client info: {}", curOAuth2Authorization.getRegisteredClientId());
+            response.sendError(HttpStatus.BAD_REQUEST.value());
             return;
         }
-        // 查询当前RP的Client注册信息
-        RegisteredClient curClient = registeredClientRepository.findById(curOAuth2Authorization.getRegisteredClientId());
-        // 找出当前所有同一sessionId的登录记录
-        List<OAuth2Authorization> allAuth = oidcAuthorizationService.findBySessionId(sessionId);
-        Map<String, OAuth2Authorization> regClientMap = new HashMap<>(allAuth.size());
-        allAuth.forEach(auth -> regClientMap.put(auth.getRegisteredClientId(), auth));
 
-        // 依次更新登出状态
-        allAuth.forEach(auth -> {
-            // 更新用户登录状态
-            auth = OAuth2Authorization.from(auth)
-                    .attribute(OAuth2Constants.AUTHORIZATION_ATTRS.LOGIN_STATE, LoginStateEnum.LOGOUT.getCode())
-                    .build();
-            // 更新token无效状态
-            auth = invalidate(auth, auth.getRefreshToken().getToken());
-            oidcAuthorizationService.save(auth);
-            // back channel 处理（front channel暂时不实现）
-            if (!curOAuth2Authorization.getRegisteredClientId().equals(auth.getRegisteredClientId())) {
-                RegisteredClient registeredClient = registeredClientRepository.findById(auth.getRegisteredClientId());
-                if (null != registeredClient.getClientSettings() && null != registeredClient.getClientSettings().getSetting(OAuth2Constants.CLIENT_SETTINGS.BACKCHANNEL_REQUIRE)
-                        && registeredClient.getClientSettings().getSetting(OAuth2Constants.CLIENT_SETTINGS.BACKCHANNEL_REQUIRE).equals(Boolean.TRUE)) {
-                    if (null == registeredClient.getClientSettings().getSetting(OAuth2Constants.CLIENT_SETTINGS.BACKCHANNEL_LOGOUT_URI)) {
-                        log.error("Custom-Debug-log===>No back channel logout uri setting for clientId:{}", registeredClient.getId());
-                    }
-                    String backchannelLogoutUri = registeredClient.getClientSettings().getSetting(OAuth2Constants.CLIENT_SETTINGS.BACKCHANNEL_LOGOUT_URI);
-                    // 生成logout_token
-                    JWT logoutToken = generateLogoutToken(registeredClient, regClientMap.get(registeredClient.getId()));
-                    sendBackchannelLogoutRequest(backchannelLogoutUri, logoutToken);
-                }
-            }
-        });
+
+        // 是否开启SLO
+        if (!oAuth2ServerProps.getEnableOidcSLO()) {
+            log.debug("Custom-Debug-log===>用户: {} 登出，OIDC SLO未启用", authentication.getName());
+        }
+        else {
+            // OP 本地 Session 立即失效
+            new SecurityContextLogoutHandler().logout(request, response, authentication);
+
+            // 找出当前所有同一sessionId的登录记录
+            // 处理所有关联会话的登出
+            List<OAuth2Authorization> relatedAuth = oidcAuthorizationService.findBySessionId(sessionId);
+            Map<String, OAuth2Authorization> regClientMap = new HashMap<>(relatedAuth.size());
+            // 可能存在一个clientId对应多个认证，防止duplicateKey
+            relatedAuth.forEach(auth -> regClientMap.put(auth.getRegisteredClientId(), auth));
+
+            // 依次更新登出状态
+            relatedAuth.forEach(auth -> processLogout(auth, curOAuth2Authorization));
+        }
 
         // 重定向回SLO调用RP的登出回调页面
-        redirectPostLogoutRedirectUri(request, response, curClient);
-
+        response.sendRedirect(determinePostLogoutRedirectUri(request, callingClient));
     }
 
-    private void redirectPostLogoutRedirectUri(HttpServletRequest request, HttpServletResponse response, RegisteredClient registeredClient) throws IOException {
-        response.sendRedirect(this.determinePostLogoutRedirectUri(request, registeredClient));
+    /**
+     * 处理单个客户端的登出流程
+     */
+    private void processLogout(OAuth2Authorization auth, OAuth2Authorization curOAuth) {
+        // 更新用户登录状态
+        auth = OAuth2Authorization.from(auth)
+                .attribute(OAuth2Constants.AUTHORIZATION_ATTRS.LOGIN_STATE, LoginStateEnum.LOGOUT.getCode())
+                .build();
+        
+        // 更新token无效状态
+        if (null != auth.getRefreshToken()) {
+            auth = invalidate(auth, auth.getRefreshToken().getToken());
+        } else {
+            log.warn("authorization: {} missing refresh_token", auth.getId());
+        }
+        
+        // 保存更新后的授权信息
+        oidcAuthorizationService.save(auth);
+
+        // 如果不是当前客户端且需要后端通道登出
+        if (!curOAuth.getRegisteredClientId().equals(auth.getRegisteredClientId())) {
+            RegisteredClient registeredClient = registeredClientRepository.findById(auth.getRegisteredClientId());
+            Boolean backchannelRequired = registeredClient.getClientSettings().getSetting(OAuth2Constants.CLIENT_SETTINGS.BACKCHANNEL_REQUIRE);
+            String backchannelLogoutUri = registeredClient.getClientSettings().getSetting(OAuth2Constants.CLIENT_SETTINGS.BACKCHANNEL_LOGOUT_URI);
+            if (Boolean.TRUE.equals(backchannelRequired) && StringUtils.hasText(backchannelLogoutUri)) {
+                // 生成logout_token
+                JWT logoutToken = generateLogoutToken(registeredClient, auth);
+                sendBackchannelLogoutRequest(backchannelLogoutUri, logoutToken);
+            }
+        }
     }
 
     private final String STATE_PARAMETER_FORMAT = "%s?state=%s";
@@ -143,6 +150,7 @@ public class BackChannelLogoutHandler implements AuthenticationSuccessHandler {
                     postLogoutRedirectUri;
         } else {
             // 不合法或没传，使用默认值或返回错误
+            log.warn("BackChannel: post_logout_redirect_uri: {} is illegal for client: {}", postLogoutRedirectUri, registeredClient);
             return oAuth2ServerProps.getLogOutDefaultRedirectUrl();
         }
     }
@@ -185,11 +193,10 @@ public class BackChannelLogoutHandler implements AuthenticationSuccessHandler {
     }
 
     /**
-     *
      * @param authorization
      * @param token
-     * @return
      * @param <T>
+     * @return
      * @see org.springframework.security.oauth2.server.authorization.authentication.OAuth2AuthenticationProviderUtils
      */
     private <T extends AbstractOAuth2Token> OAuth2Authorization invalidate(OAuth2Authorization authorization, T token) {
